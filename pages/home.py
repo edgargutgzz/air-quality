d994ce3a2125ef06001c48a6160779b9063084da
Edgar Gutierrez <edgargu47@gmail.com>
2023-05-10 19:16:22 -0600

    add colors to scatter plot

--- pages/home.py
+++ pages/home.py
@@ -88,32 +88,60 @@ scatter_dataframe['color_label'] = scatter_dataframe['avg_pm25'].apply(assign_co
 scatter_dataframe['sensor_count'] = range(1, len(scatter_dataframe) + 1)
 scatter_dataframe['municipio_order'] = scatter_dataframe.groupby('municipio').ngroup()
 
+# Update the column names
+scatter_dataframe.rename(columns={"color_label": "Calidad del Aire", "avg_pm25": "PM2.5", "municipio": "Municipio"}, inplace=True)
+
+
 scatter_fig = px.scatter(
     scatter_dataframe,
-    x="avg_pm25",
-    y='municipio',
-    color="color_label",
+    x="PM2.5",
+    y='Municipio',
+    color="Calidad del Aire",
     color_discrete_sequence=["#00FF00", "#0000FF", "#FFFF00", "#FFA500", "#FF0000"],
     title=None,
-    hover_name='municipio'
+    hover_name='nombre'
 )
+
+scatter_fig.update_traces(
+    marker=dict(
+        line=dict(
+            width=1,  # Modify the width of the border here
+            color='grey'  # Set the color of the border
+        )
+    )
+)
+
 
 scatter_fig.update_layout(
     xaxis_title=None,
     yaxis_title=None,
-    showlegend=False
+    showlegend=False,
 )
 
 scatter_fig.update_layout(
-    height=600,  # Increase the height of the plot
+    height=600, 
     margin=dict(l=20, r=20, t=20, b=20),
-    yaxis=dict(  # Increase the distance between tick labels
+    yaxis=dict(  
         tickmode="array",
-        tickvals=scatter_dataframe["municipio"],
-        ticktext=scatter_dataframe["municipio"],
-        tickfont=dict(size=10),
+        tickvals=scatter_dataframe["Municipio"],
+        ticktext=["   " + label + "   " for label in scatter_dataframe["Municipio"]],
+        tickfont=dict(size=14),
         tickangle=0,
-        ticklen=10
+        ticklen=10,
+        tickcolor='white'
+    ),
+    xaxis=dict(  
+        tickmode="auto",
+        tickfont=dict(size=14),
+        tickangle=0,
+        ticklen=10,
+        tickcolor='white',
+        ticklabelmode="period",
+        ticklabelposition="outside",
+        ticksuffix="   ",
+        tickvals=scatter_dataframe["PM2.5"],
+        ticktext=["   " + str(label) + "   " for label in scatter_dataframe["PM2.5"]],
+        dtick=1 # add this dtick value to adjust tick spacing
     )
 )
 
